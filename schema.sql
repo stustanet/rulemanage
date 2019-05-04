d7a65ef51aec105aa0000628003e35e4335316d0
Julien Schmidt <js@stusta.net>
2019-05-04 16:21:20 +0200

    improve rule_comment schema

--- schema.sql
+++ schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE rule (
 
 CREATE TABLE rule_comment (
     id serial PRIMARY KEY NOT NULL,
-    sid integer,
+    sid integer REFERENCES rule(sid),
     rev smallint,
+    commented_at timestamp NOT NULL DEFAULT NOW(),
     comment text NOT NULL
 );
